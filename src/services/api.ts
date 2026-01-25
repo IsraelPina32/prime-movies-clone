1283bbe068dce8af8d4cab0bf9762928185fa86a
IsraelPina32 <israelpina047@gmail.com>
2026-01-25 18:44:19 -0300

    feat(services): setup axios instance with env variables
    - Configures base axios instance for OMDb API.
    - Implements request interceptors for automated API Key injection.
    -Ensures security by using .env.local for senstive credentials.
    - Adds environment file patterns to .gitiignore.

---
+++ src/services/api.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+/**    
+ * Global Axios instance configuration
+ * Centralizes API base URL and common parameters
+*/
+
+const api = axios.create({
+    baseURL: 'https://www.omdapi.com/',
+    timeout: 10000, // 10 seconds timeout
+});
+
+/**
+ * Request Interceptor
+ * Automatically injects the API key into every request to avoid repetion.
+ */
+
+api.interceptors.request.use((config) => {
+
+    const apiKey = import.meta.env.Vite_OMBD_API_KEY;
+
+    if(!apiKey) {
+        console.log('API key is missing! check your .env.local file.');
+        
+    }
+    config.params = {
+        ...config.params,
+        apikey: apiKey,
+    };
+    return config;
+});
+
+export default api;
