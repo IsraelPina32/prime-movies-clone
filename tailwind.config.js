7a74afccbacba9977d838e35798e719293cfc370
IsraelPina32 <israelpina047@gmail.com>
2026-01-28 13:52:29 -0300

    feat(theme): define branc palette (prime-bg, prime-card, prime-blue)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        // Cores oficiais da interface Prime Video
-        brand: {
-          background: "#0f171e",
-          card: "#1a242f",
-          blue: "#1a98ff",
-          text: "#f2f4f6",
-        }
+        'prime-bg': '#0f171e',      // background oficial.
+        'prime-card': '#1a242f',    // background of cards.
+        'prime-blue': '#00a8e1',    // Blue of the btns/links.
       },
     },
   },
