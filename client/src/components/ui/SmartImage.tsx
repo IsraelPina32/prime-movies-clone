fb8b5df0fb5bce8bf1cd5cb66762636d09524711
IsraelPina32 <israelpina047@gmail.com>
2026-02-09 14:29:04 -0300

    refactor(ui): implement resilient URL resolution and falback in SmartImage.

--- client/src/components/ui/SmartImage.tsx
+++ client/src/components/ui/SmartImage.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { MovieSkeleton } from '../movies/MovieSkeleton';
+import  noImagePlaceholder from "../../assets/no-poster.svg"
 
 interface SmartImageProps {
-  src: string;
+  src: string | null;
   alt: string;
   className?: string;
+  opacity?: number;
 }
 
 export const SmartImage: React.FC<SmartImageProps> = ({ src, alt, className }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [hasError, setHasError] = useState(false);
+
+  const getImageUrl = (path: string | null) => {
+    if (!path || hasError) return noImagePlaceholder;
+    if (path.startsWith('http')) return path;
+    return `https://image.tmdb.org/t/p/w500${path}`;
+  };
+
+  const finalSrc = getImageUrl(src);
 
   return (
     <div className="relative w-full h-full bg-slate-800 overflow-hidden">
@@ -26,13 +36,16 @@ export const SmartImage: React.FC<SmartImageProps> = ({ src, alt, className }) =
       </AnimatePresence>
 
       <motion.img
-        src={hasError ? 'https://via.placeholder.com/300x450?text=Erro' : src}
+        src={finalSrc}
         alt={alt}
         initial={{ opacity: 0 }}
         animate={{ opacity: isLoaded ? 1 : 0 }}
         transition={{ duration: 0.5, ease: "easeIn" }}
         onLoad={() => setIsLoaded(true)}
-        onError={() => setHasError(true)}
+        onError={() => {
+          setHasError(true)
+          setIsLoaded(true);
+        }}
         className={`${className} ${isLoaded ? 'scale-100' : 'scale-110'} transition-transform duration-700`}
         loading="lazy"
       />
