558e9d146b395ccf5f16547aa68f4868dcc251b1
IsraelPina32 <israelpina047@gmail.com>
2026-02-09 11:03:44 -0300

    feat(types):  add extended properties to Movie type.

---
+++ client/src/types/movie.ts
@@ -0,0 +1,26 @@
+
+export interface Movie {
+    readonly id: string,
+    Title: string;
+    Year: string;
+    imdbID: string;
+    Type: string;
+    Poster: string | null;
+};
+
+export interface MovieResponse {
+    Search?: Movie[];
+    totalResults?: string;
+    Response: string;
+    Error?: string;
+};
+
+export interface MovieDetail extends Movie {
+    Plot: string;
+    Genre: string;
+    Director: string;
+    Actors: string;
+    imdRating: string;
+    Runtime: string;
+    Released: string;
+}
