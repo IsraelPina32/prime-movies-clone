90ade400b640b208e59a097b1e89665e328fbc8e
IsraelPina32 <israelpina047@gmail.com>
2026-02-11 11:42:13 -0300

    choreclient):  implement data formatting and sanitization utilities.

---
+++ client/src/utils/formatters.ts
@@ -0,0 +1,16 @@
+export const formatRuntime = (runtimeStr: string | undefined): string => {
+    if(!runtimeStr || runtimeStr === "N/A") return '--';
+
+    const totalMinutes = parseInt(runtimeStr.replace(/\D/g, ''), 10);
+
+    if(isNaN(totalMinutes)) return runtimeStr;
+
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+
+    if (hours === 0) return `${minutes} min`;
+
+    const minutesPart = minutes > 0 ? ` ${minutes} min` : "";
+
+    return `${hours}h${minutesPart}`;
+}
