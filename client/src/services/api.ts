7fcae21b1c74bc14a97e1ef70c3a12ba34904a98
IsraelPina32 <israelpina047@gmail.com>
2026-02-12 15:27:51 -0300

    refactor(front) : standardise api client with enviroment variales and timeout

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -5,11 +5,14 @@ import axios from "axios";
  * Centralizes API base URL and common parameters
 */
 
-const API_BASE_URL = import.meta.env.API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://localhost:3001/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
     timeout: 10000, // 10 seconds timeout
+    headers: {
+        'Content-Type': 'aplication/json'
+    }
 });
 
 /**
