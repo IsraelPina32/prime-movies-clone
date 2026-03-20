517b4fecbe240f6ff25c04154ab0637fadfa3857
IsraelPina32 <israelpina047@gmail.com>
2026-03-20 11:01:10 -0300

    refactor(home): decouple movie grid logic into FilteredMovieGrid component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,10 @@
-import { MovieGridSkeleton } from "../components/movies/MovieGridSkeleton";
-import { ErrorState } from "../components/ui/ErrorState";
-import { MovieCard } from "../components/movies/MovieCard"
 import { useMovieContext } from "../context/MovieContext";
 import { Pagination } from "../components/Pagination";
 import { useEffect } from "react";
+import { FilteredMovieGrid } from "../components/layout/components/FilteredMovieGrid";
 
 export const Home = () => {
-    const { movies, loading, error, searchTerm, searchMovies, selectedGenre, selectedYear, currentPage, setCurrentPage, totalResults } = useMovieContext();
+    const { movies, searchTerm, searchMovies, selectedGenre, selectedYear, currentPage, setCurrentPage, totalResults } = useMovieContext();
 
     useEffect(() => {
       if(searchTerm) {
@@ -18,31 +16,12 @@ export const Home = () => {
       setCurrentPage(newPage);
       window.scrollTo({ top: 0, behavior: 'smooth'})
     }
+
     return (
     <main className="max-w-[1440px] mx-auto px-10 py-6">
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 p-3 pt-4">
-        {loading && <MovieGridSkeleton count={10} isLoading/>}
-       {!loading && error && (
-        <div className="col-span-full py-20">
-          <ErrorState message={error} onRetry={() => searchMovies({ query: searchTerm})}/>
-        </div>
-        )}
-        {!loading && !error && (movies || []).map((movie => (
-          <MovieCard  key={movie.id} movie={movie}/>
-        )))
-        }
-        {!loading && !error && searchTerm && movies.length === 0 && (
-          <div className="col-span-full text-center py-20 text-gray-400">
-            <p className="text-xl">Nenhum filme encontrado para "<span className="text-white">{searchTerm}</span>"</p>
-            <p className="text-sm mt-2">Tente digitar o nome de outro filme.</p>
-          </div>
-        )}
-        {!loading && !error && !searchTerm && (
-          <div className="col-span-full text-center py-20 text-gray-500">
-            <p className="text-2xl font-light italic">Pesquise por seus filmes favoritos acima...</p>
-          </div>
-        )}
-      </div>
+
+      <FilteredMovieGrid/>
+   
       { movies.length > 0 ? (
                 <Pagination 
                 currentPage={currentPage} totalResults={totalResults} onPageChange={handlePageChange}
