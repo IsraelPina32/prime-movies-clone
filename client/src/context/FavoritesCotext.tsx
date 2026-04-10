dd4a122ccff8120959e4b443e7f8a95ce71a67d7
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-04-10 18:08:18 -0300

    feat(client) : implement FavoritesContext with localStorage persistence - Implement lazy state initialization for optimized localStorage access - Add toggleFavorite and isFavorite logic with Typescript enforcement - Prepare architectural foundation for Favorites page and UI interactions

---
+++ client/src/context/FavoritesCotext.tsx
@@ -0,0 +1,50 @@
+import  { createContext, useContext, useState, useEffect, type ReactNode} from "react";
+
+
+interface Movie {
+    imdbID: string;
+    Title: string;
+    Year: string;
+    Poster: string;
+    Type: string;
+}
+
+interface FavoritesContextData {
+    favorites: Movie[];
+    toggleFavorite: (movie: Movie) => void;
+    isFavorite: (movieID: string) => boolean;
+}
+
+const FavoritesContext = createContext<FavoritesContextData>({} as FavoritesContextData);
+
+export const FavoritesProvider = ({ children }: { children: ReactNode}) => {
+        const [favorites, setFavorites] = useState<Movie[]>(() => {
+            const savedFavorites = localStorage.getItem("@Movies:favorites");
+            return savedFavorites ? JSON.parse(savedFavorites) : []
+        });
+
+        useEffect(() => {
+           localStorage.setItem("@Movies:favorites", JSON.stringify(favorites));
+        }, [favorites]);
+
+
+        const toggleFavorite = (movie: Movie) => {
+            setFavorites((prevFavorites) => {
+                const isAlreadyFavorite = prevFavorites.some((fav) => fav.imdbID === movie.imdbID);
+                if (isAlreadyFavorite) {
+                    return prevFavorites.filter((fav) => fav.imdbID !== movie.imdbID);
+                };
+                return [...prevFavorites, movie];
+            });
+        };
+
+        const isFavorite = (id: string) => favorites.some((fav) => fav.imdbID === id);
+
+    return (
+    <FavoritesContext.Provider value={{ favorites, toggleFavorite, isFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export const useFavorites = () => useContext(FavoritesContext);
