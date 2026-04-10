130b818a0f2937a6e3aeba71088cf76fd5aaf7c5
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-04-10 18:31:59 -0300

    build(client): wrap application with FavoritesProvider, add new component FavoritesPage.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { MovieDetailsPage } from "./pages/MovieDetails/MovieDetailsPage";
 import { MovieProvider } from "./context/MovieContext";
+import { FavoritesProvider } from "./context/FavoritesCotext";
+import { FavoritesPage } from "./pages/Favorites/FavoritesPage";
 function App() {
   
   return (
     <MovieProvider >
+      <FavoritesProvider>
         <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/movie/:id" element={<MovieDetailsPage/>}/>
+          <Route path="/favorites" element={<FavoritesPage/>}/>
         </Routes>
+      </FavoritesProvider>
     </MovieProvider>
   );
 }
