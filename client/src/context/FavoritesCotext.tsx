8388f6bab2ef2ca9834ec31dcbe237ac32cfa4a7
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-04-13 19:11:14 -0300

    refactor(context): finalize FavoritesProvider orchestration - Implement Provider using the decoupled useFavoritesLogic hook

--- client/src/context/FavoritesCotext.tsx
+++ client/src/context/FavoritesCotext.tsx
@@ -1,43 +1,27 @@
-import  { createContext, useContext, useState, useEffect, type ReactNode} from "react";
-
+import { createContext, type ReactNode } from "react";
 import { type Movie } from "../types/movie";
+import { useFavoritesInternalLogic } from "../hooks/useFavoritesLogic";
 
 interface FavoritesContextData {
-    favorites: Movie[];
-    toggleFavorite: (movie: Movie) => void;
-    isFavorite: (movieID: string) => boolean;
-}
+  favorites: Movie[];
+  toggleFavorite: (movie: Movie) => void;
+  isFavorite: (movieID: string) => boolean;
+} 
 
-const FavoritesContext = createContext<FavoritesContextData>({} as FavoritesContextData);
-
-export const FavoritesProvider = ({ children }: { children: ReactNode}) => {
-        const [favorites, setFavorites] = useState<Movie[]>(() => {
-            const savedFavorites = localStorage.getItem("@Movies:favorites");
-            return savedFavorites ? JSON.parse(savedFavorites) : []
-        });
-
-        useEffect(() => {
-           localStorage.setItem("@Movies:favorites", JSON.stringify(favorites));
-        }, [favorites]);
+export const FavoritesContext = createContext<FavoritesContextData | undefined>(undefined);
 
 
-        const toggleFavorite = (movie: Movie) => {
-            setFavorites((prevFavorites) => {
-                const isAlreadyFavorite = prevFavorites.some((fav) => fav.imdbID === movie.imdbID);
-                if (isAlreadyFavorite) {
-                    return prevFavorites.filter((fav) => fav.imdbID !== movie.imdbID);
-                };
-                return [...prevFavorites, movie];
-            });
-        };
+export const FavoritesProvider = ({ children }: { children: ReactNode }) => {
+  const logic = useFavoritesInternalLogic();
 
-        const isFavorite = (id: string) => favorites.some((fav) => fav.imdbID === id);
-
-    return (
-    <FavoritesContext.Provider value={{ favorites, toggleFavorite, isFavorite }}>
+  return (
+    <FavoritesContext.Provider 
+      value={{ 
+        ...logic, 
+        isFavorite: (id) => logic.favorites.some((f) => f.imdbID === id) 
+      }}
+    >
       {children}
     </FavoritesContext.Provider>
   );
 };
-
-export const useFavorites = () => useContext(FavoritesContext);
