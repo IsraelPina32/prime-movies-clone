57ffab2b87fb1ecdff09e11ac5e0c706047f75dc
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-05-20 17:32:00 -0300

    config(env): setup environment variable manager and validation schema.

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,13 +1,15 @@
 import datenv from 'dotenv';
 import path from 'path';
 
-datenv.config();
+datenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 export const config = {
     port: process.env.PORT || 3001,
     omdb: {
         apiKey: process.env.OMDB_API_KEY?.trim() || '',
-        baseURL: 'https://www.omdbapi.com/'
+        baseUrl: 'https://www.omdbapi.com/'
     },
+    nodeEnv: process.env.NODE_ENV || 'development',
     isProduction: process.env.NODE_ENV === 'production',
+    
 };
