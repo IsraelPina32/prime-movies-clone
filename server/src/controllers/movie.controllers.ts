a869a034d3ba1d7e1c99facdbf5641e7c7f6c204
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-05-20 17:30:26 -0300

    feat(controllers) : add getMovies and getMoviesDetails endpoint handlers

--- server/src/controllers/movie.controllers.ts
+++ server/src/controllers/movie.controllers.ts
@@ -1,22 +1,45 @@
-import {  Request ,Response} from 'express';
+import {  Request, Response} from 'express';
 import { MovieService } from '@/services/movie.services';
-
 
 const movieService = new MovieService();
 
 export const getMovies = async (req: Request, res: Response) => {
     try {
-        const { s } = req.query;
+        const { query, page } = req.query;
 
-        if(!s || typeof s !== 'string') {
+        if(!query || typeof query !== 'string') {
             return res.status(400).json({ error: 'Query parameter "s" is required and must be a string' });
         };
 
-        const movies = await movieService.searchMovies(s);
-        return res.json(movies);
+        const result = await movieService.searchMovies(query, Number(page) || 1);
+        return res.json({
+            movies: result.movies,
+            totalResults: result.total
+        });
 
     } catch (error: any) {
         console.error(' [MovieController]:', error.message);
         return res.status(500).json({ error: 'An error occurred while fetching movies' });
     };
+};
+
+export const getMovieDetails = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params; 
+        if (typeof id !== 'string') {
+            return res.status(400).json({ error: 'Movie ID is required' });
+        }
+
+        const movie = await movieService.getMovieDetails(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        return res.json(movie);
+
+    } catch (error: any) {
+        console.error(' [MovieController Details]:', error.message);
+        return res.status(500).json({ error: 'An error occurred while fetching movie details' });
+    }
 };
