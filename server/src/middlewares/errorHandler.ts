5da7eee5d76a2b002e81ee782bde6fed5b390ad7
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-05-22 20:15:41 -0300

    fix(server): strongly type error handler middleware and update cloud pipeline install

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,11 +1,11 @@
 import { ErrorRequestHandler } from 'express';
 
-
-export const errorHandler: ErrorRequestHandler = (err, req, res, next): void => {
-    console.error(' [Global Error]:', err.message);
-
-    res.status(500).json({
-        error: err.message || 'Internal Server Error',
-        stack: process.env.NODE_ENV === 'production' ? '🔒' : err.stack
-    });
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  
+  res.status(statusCode).json({
+    status: 'error',
+    statusCode,
+    message: err.message || 'Internal Server Error',
+  });
 };
